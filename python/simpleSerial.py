508db939fe1b5c893e92e0fd7c64b8254dd6aa53
Thomas D. Kryger <tdkryger@akvariet.dk>
2015-03-01 03:16:53 +0100

    Code to connect to different serial port

--- python/simpleSerial.py
+++ python/simpleSerial.py
@@ -1,21 +1,54 @@
 import serial
 import time
 
-#serName = "/dev/ttyACM0"
-serName = "COM4"
+locations=[
+    'COM3',
+    'COM4',
+    '/dev/ttyACM0'
+    '/dev/ttyUSB0',
+    '/dev/ttyUSB1',
+    '/dev/ttyUSB2',
+    '/dev/ttyUSB3',
+    '/dev/ttyS0',
+    '/dev/ttyS1',
+    '/dev/ttyS2',
+    '/dev/ttyS3']
 
-ser =  serial.Serial(serName, 115200, timeout=1)
+def local_time_offset(t=None):
+    """Return offset of local zone from GMT, either at present or at time t."""
+    # python2.3 localtime() can't take None
+    if t is None:
+        t = time.time()
+
+    if time.localtime(t).tm_isdst and time.daylight:
+        return -time.altzone
+    else:
+        return -time.timezone
+
+
+#serName = "COM4"
+#serName = "/dev/ttyACM0"
+
+for device in locations:
+    try:
+        print "Trying...",device
+        arduino = serial.Serial(device, 115200, timeout=1)
+        break
+    except:
+        print "Failed to connect on",device
+
+
+
+#arduino =  serial.Serial(serName, 115200, timeout=1)
 while True:
-    serialString = ser.readline()
+    serialString = arduino.readline()
     if serialString:
-        print("\tDatastring: " + serialString)
+        print("Datastring: " + serialString)
         if serialString.startswith('t'):
-            # It returns UTC. Need localtime
-            ticks = time.time()
-            returnString = 'T' + str(int(ticks)) + "    "
-            print("Returnstring: " + returnString)
-            print("Returnstring: " + unicode(returnString))
-            ser.write(returnString)
-            #ser.flush()
+            ticks = time.time() + local_time_offset()
+            returnString = 'T' + str(int(ticks)) + "TTTTT"
+            print("\t\tReturnstring: " + returnString)
+            arduino.write(returnString)
+            #arduino.flush()
             
         
